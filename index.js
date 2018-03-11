7f8f5b5959d711613d961f337e986a63ec79a0a0
lependu <lependu@protonmail.com>
2018-03-11 12:26:43 +0100

    Updates to fastify@1.x

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const loadScripts = (path) => {
 }
 
 module.exports = fp(fastifyLured, {
-  fastify: '>=1.0.0-rc.1',
+  fastify: '>=1.0.0',
   name: 'fastify-lured',
   decorators: {
     fastify: ['redis']
